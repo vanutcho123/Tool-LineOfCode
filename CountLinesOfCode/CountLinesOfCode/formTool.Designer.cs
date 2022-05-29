c209dddfd28f79b215412c7bfe297e6500add6fb
vanutcho123 <hainguyen.061001@gmail.com>
2022-05-29 14:27:16 +0700

    Added new files

---
+++ CountLinesOfCode/CountLinesOfCode/formTool.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CountLinesOfCode
+{
+    partial class formTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLinkFile = new System.Windows.Forms.TextBox();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_LineOfCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_NumberOfFunction = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.btn_Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Link File :";
+            // 
+            // txtLinkFile
+            // 
+            this.txtLinkFile.Location = new System.Drawing.Point(112, 22);
+            this.txtLinkFile.Name = "txtLinkFile";
+            this.txtLinkFile.Size = new System.Drawing.Size(295, 20);
+            this.txtLinkFile.TabIndex = 1;
+            // 
+            // btn_select
+            // 
+            this.btn_select.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_select.Location = new System.Drawing.Point(433, 14);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(103, 28);
+            this.btn_select.TabIndex = 2;
+            this.btn_select.Text = "Select File";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(552, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Count Function";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(28, 108);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(641, 214);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Line Of Code :";
+            // 
+            // txt_LineOfCode
+            // 
+            this.txt_LineOfCode.Location = new System.Drawing.Point(136, 347);
+            this.txt_LineOfCode.Name = "txt_LineOfCode";
+            this.txt_LineOfCode.Size = new System.Drawing.Size(122, 20);
+            this.txt_LineOfCode.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(336, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Number Of Function :";
+            // 
+            // txt_NumberOfFunction
+            // 
+            this.txt_NumberOfFunction.Location = new System.Drawing.Point(508, 344);
+            this.txt_NumberOfFunction.Name = "txt_NumberOfFunction";
+            this.txt_NumberOfFunction.Size = new System.Drawing.Size(162, 20);
+            this.txt_NumberOfFunction.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.Location = new System.Drawing.Point(552, 399);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(117, 25);
+            this.btn_Thoat.TabIndex = 9;
+            this.btn_Thoat.Text = "Exit";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // btn_Reset
+            // 
+            this.btn_Reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Reset.Location = new System.Drawing.Point(112, 62);
+            this.btn_Reset.Name = "btn_Reset";
+            this.btn_Reset.Size = new System.Drawing.Size(103, 25);
+            this.btn_Reset.TabIndex = 10;
+            this.btn_Reset.Text = "Reset";
+            this.btn_Reset.UseVisualStyleBackColor = true;
+            this.btn_Reset.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // formTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.Controls.Add(this.btn_Reset);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.txt_NumberOfFunction);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_LineOfCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.txtLinkFile);
+            this.Controls.Add(this.label1);
+            this.Name = "formTool";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Tool LineOfCode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLinkFile;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_LineOfCode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_NumberOfFunction;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Button btn_Reset;
+    }
+}
+
---
+++ CountLinesOfCode/CountLinesOfCode/formTool.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CountLinesOfCode
+{
+    public partial class formTool : Form
+    {
+        public formTool()
+        {
+            InitializeComponent();
+        }
+
+        //Select File
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.FileName;
+            txtLinkFile.Text = fileName.ToString();
+        }
+
+        //Check xem từng dòng có phải là dòng chứa function
+        //name hay không
+        public bool IsFunction(string line)
+        {
+            if (line.Contains("//") || line.Contains("/*") || line.Contains("*/"))
+                return false;
+
+            var lines = line.Split(' ');
+            var types = new List<string>
+            {
+                "void", "int", "long", "float", "short", "char", "double", "bool",
+                "public", "private", "static", "internal", "protected", "protected internal"
+            };
+
+            if (lines.Any(l => types.Contains(l)) && line.EndsWith(")"))
+            {
+                if (!line.Contains(";"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        //kiểm tra xem số lượng dòng code của mỗi function.
+        public int CountLineOfCodeFunction(string functionName, string[] data)
+        {
+            int count = 0;
+            int start = 0, end = 0;
+
+            foreach (var d in data)
+            {
+                if (d == functionName)
+                {
+                    count++;
+                    continue;
+                }
+
+                if (count > 0)
+                {
+
+                    if (d.Contains("{"))
+                    {
+                        start++;
+                        count++;
+                        continue;
+                    }
+
+                    if (d.Contains("}"))
+                    {
+                        end++;
+                        count++;
+                        continue;
+                    }
+
+                    if (start == end)
+                        break;
+
+                    if (d != null)
+                        count++;
+                }
+
+
+            }
+
+            return count;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var path = txtLinkFile.Text; // Get đường dẫn file từ text box 1
+
+            int numberOfFunction = 0; // Đếm số lượng function trong file
+            int lineOfCode = 0; // Đếm số dòng code trong file
+
+            var data = File.ReadAllLines(path); // Đọc toàn bộ dòng trong file, trả về string[]
+            Dictionary<string, int> result = new Dictionary<string, int>(); // Lưu cặp giá trị: string (line function name), int (số lines code của function)
+
+            foreach (var line in data)
+            {
+                if (IsFunction(line)) // == true: đây là line function name
+                {
+                    numberOfFunction++; // Tăng biến đếm lên 1.
+                    int lineCode = CountLineOfCodeFunction(line, data); // Đếm số dòng code của function
+                    result.Add(line, lineCode); // Add cặp giá trị: functionName, lineCode
+                }
+
+                lineOfCode++; // Tăng biến đếm tổng line code lên 1
+            }
+
+            txt_NumberOfFunction.Text = numberOfFunction.ToString(); // Hiển thị số lượng function ra text box 3
+            txt_LineOfCode.Text = lineOfCode.ToString(); // Hiển thị tổng số line code ra text box 2
+
+            // In dữ liệu ra list view với 2 columns
+            listView1.View = View.Details;
+            listView1.Columns.Add("Function Name", 500); // Add column 1 với name và độ rộng
+            listView1.Columns.Add("Line Of Code", 100); // Add column 2 với name và độ rộng
+
+            // Chèn dữ liệu vào list view
+            foreach (var item in result)
+            {
+                // Add từng mảng giá trị tương ứng
+                listView1.Items.Add(new ListViewItem(new string[] { item.Key, item.Value.ToString() }));
+            }
+
+            listView1.GridLines = true;
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            txtLinkFile.Text = "";
+            listView1.Items.Clear();
+        }
+
+    }
+}
